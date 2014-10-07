732084778a6d3430783a78146642b6713277d03e
TonyDon <hongdeviltxd@163.com>
2014-10-08 00:12:10 +0800

    update demo action

--- src/main/java/com/uuola/txcms/action/UserInfoAction.java
+++ src/main/java/com/uuola/txcms/action/UserInfoAction.java
@@ -6,10 +6,6 @@
 
 package com.uuola.txcms.action;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.lang.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,17 +13,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.uuola.commons.constant.HTTP_STATUS_CODE;
-import com.uuola.commons.exception.BusinessException;
-import com.uuola.commons.exception.BusinessExceptionMessageProvider;
 import com.uuola.txcms.base.dto.UserInfoDTO;
 import com.uuola.txcms.base.query.UserInfoQuery;
 import com.uuola.txcms.base.service.UserInfoService;
 import com.uuola.txcms.base.service.UserRegService;
 import com.uuola.txweb.framework.action.BaseQueryAction;
+import com.uuola.txweb.framework.action.methods.QueryCallbackHandler;
+import com.uuola.txweb.framework.action.methods.UpdateCallbackHandler;
 import com.uuola.txweb.framework.dto.PageDTO;
+import com.uuola.txweb.framework.dto.ValidateDTO;
 import com.uuola.txweb.framework.query.BaseQuery;
-import com.uuola.txweb.framework.query.QueryCallbackHandler;
 
 
 /**
@@ -55,18 +50,14 @@ public class UserInfoAction extends BaseQueryAction {
      */
     @RequestMapping(value = "/search", method = RequestMethod.GET)
     public ModelAndView search(UserInfoQuery query, ServletWebRequest webRequest) {
-
         ModelAndView model = executeQuery(webRequest, query, new QueryCallbackHandler() {
-
             @Override
             public PageDTO doQuery(BaseQuery query) {
                 return userInfoService.fetchByRange(query);
             }
-
         });
         model.setViewName(this.getViewName("search"));
         return model;
-
     }
     
     /**
@@ -77,31 +68,17 @@ public class UserInfoAction extends BaseQueryAction {
      */
     @RequestMapping(value="/add", method=RequestMethod.POST)
     public ModelAndView add(UserInfoDTO userInfoDTO, ServletWebRequest webRequest) {
-        ModelAndView model = new ModelAndView();
-        List<String> errors = new ArrayList<String>();
-        if (!userInfoDTO.validatePass()) {
-            errors.addAll(getErrors(userInfoDTO));
-        } else {
-            // 保存用户基本信息
-            try {
-                userRegService.saveNewUserInfo(userInfoDTO);
-            }catch(BusinessException be){
-                errors.add(BusinessExceptionMessageProvider.getMessage(be));
-                log.error("UserInfoAction.add()", be);
-            }catch(Exception e){
-                errors.add(ExceptionUtils.getRootCauseMessage(e));
-                log.error("UserInfoAction.add()", e);
+        ModelAndView model = executeUpdate(webRequest, userInfoDTO, new UpdateCallbackHandler<Object>() {
+            @Override
+            public Object doUpdate(ValidateDTO userInfoDTO) {
+                userRegService.saveNewUserInfo((UserInfoDTO)userInfoDTO);
+                return null;
             }
-        }
-        // 重置属性为null
+        });
+        
+        // 重置关键属性为null
         userInfoDTO.reset();
-        model.addObject(ERRORS_ATTR, errors);
         model.setViewName(getViewName("add"));
-        
-        if(!errors.isEmpty()){
-            webRequest.getResponse().setStatus(HTTP_STATUS_CODE.SC_BZ_ERROR);
-        }
-        
         return model;
     }
 
