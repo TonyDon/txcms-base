ae1d7034330f99c5948f7c80fc7399660a1eeb8e
tangxiaodong <tangxiaodong@NHWHORNB70006.yihaodian.com>
2015-11-03 16:17:52 +0800

    update

---
+++ src/main/java/com/uuola/txcms/manager/action/WebGameAction.java
@@ -0,0 +1,32 @@
+/*
+ * @(#)WebGameAction.java 2015年11月3日
+ * 
+ * Copy Right@ uuola
+ */ 
+
+package com.uuola.txcms.manager.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uuola.txweb.framework.action.BaseAction;
+
+
+/**
+ * <pre>
+ * WEB游戏
+ * @author tangxiaodong
+ * 创建日期: 2015年11月3日
+ * </pre>
+ */
+@Controller
+@RequestMapping("/manager/app/webgame")
+public class WebGameAction extends BaseAction {
+
+    @RequestMapping(value="/addpage", method=RequestMethod.GET)
+    public ModelAndView addPage(){
+        return this.makeModelView("addpage");
+    }
+}
