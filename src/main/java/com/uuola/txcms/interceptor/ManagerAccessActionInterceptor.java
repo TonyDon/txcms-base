c5d53f7f7bf77301c3a59172ba2d4cd5f419fb34
TonyDon <hongdeviltxd@163.com>
2016-01-19 22:17:44 +0800

    update

---
+++ src/main/java/com/uuola/txcms/interceptor/ManagerAccessActionInterceptor.java
@@ -0,0 +1,47 @@
+/*
+ * @(#)UserAccessActionInterceptor.java 2014-4-20
+ * 
+ * Copy Right@ uuola
+ */ 
+
+package com.uuola.txcms.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.uuola.txcms.component.SessionUtil;
+
+
+/**
+ * <pre>
+ *
+ * @author tangxiaodong
+ * 创建日期: 2014-4-20
+ * </pre>
+ */
+public class ManagerAccessActionInterceptor extends HandlerInterceptorAdapter {
+    
+    private String redirectUrl;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(handler instanceof HandlerMethod && null == SessionUtil.getAdmin()){
+            response.sendRedirect(request.getContextPath() + redirectUrl);                 
+            return false;
+        }
+        return true;
+    }
+
+    
+    public String getRedirectUrl() {
+        return redirectUrl;
+    }
+
+    
+    public void setRedirectUrl(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+}
